bf41070ecee8dd6fd818c1709f5248bbd0f1adea
ethancaplea <ethan.caplea@processanddata.com>
2025-04-02 01:00:40 -0400

    - Added image assets - Created index page - Created contact page - Created defuault layout with basic header and footer - Set up app.vue to use pages and default layout

--- app.vue
+++ app.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+
+    <!-- This is just the main entry point for the application. See layouts/default.vue for more info -->
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+
 </template>
---
+++ layouts/default.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+
+        <!-- header -->
+        <header>
+            <nav>
+                <ul>
+                    <li><nuxt-link to="/">Home</nuxt-link></li>
+                    <li><nuxt-link to="/contact">Contact</nuxt-link></li>
+                </ul>
+            </nav>
+        </header>
+
+        <!-- page contents -->
+        <main>
+            <slot />
+        </main>
+
+        <!-- footer -->
+        <footer>
+            <p>FOOTER </p>
+        </footer>
+
+    </div>
+</template>
