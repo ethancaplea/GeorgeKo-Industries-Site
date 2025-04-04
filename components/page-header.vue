d6c1945adae852aaa7b0355f1789e55a82b79258
ethancaplea <ethan.caplea@processanddata.com>
2025-04-04 00:43:23 -0400

    - Header changes - Started on home page

--- components/page-header.vue
+++ components/page-header.vue
@@ -2,14 +2,15 @@
     <div class="flex">
         <div class="p-3 border">
             <a href="/">
-                <img src="/assets/images/logo.jpg" alt="Logo" class="w-2/3" />
+                <img src="/assets/images/logo.jpg" alt="Logo" class="w-[90px]" />
             </a>
         </div>
         <nav class=" flex items-center grow border">
-            <ul class="flex justify-evenly grow border text-[24px]">
+            <ul class="flex justify-evenly grow border text-[12px]">
                 <li><a href="/">Home</a></li>
-                <li><a href="/">Faculties</a></li>
                 <li><a href="/about">About</a></li>
+                <li><a href="/">Services</a></li>
+                <li><a href="/">Faculties</a></li>
                 <li><a href="/contact">Contact</a></li>
             </ul>
         </nav>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,27 @@
 <template>
-    <div>
-        Home
+        <!-- Section 1 -->
+        <div class="relative">
+        <!-- The background -->
+        <div class="flex absolute inset-0">
+            <img src="assets/images/machine.jpg" alt="background" class="basis-1/2 object-cover"/>
+            <div class="basis-1/2 bg-blue-100">
+                TEST TEST
+            </div>
+        </div>
+
+        <!-- The content -->
+        <div class="relative z-10">
+            <ul>
+                <li class="text-[40px] text-center font-bold text-white">TEST</li>
+                <li class="text-[20px] text-center font-bold text-white">TEST</li>
+                <li class="text-[20px] text-center font-bold text-white">TEST</li>
+                <li class="text-[20px] text-center font-bold text-white">TEST</li>
+                <li class="text-[20px] text-center font-bold text-white">TEST</li>
+                <li class="text-[20px] text-center font-bold text-white">TEST</li>
+                <li class="text-[20px] text-center font-bold text-white">TEST</li>
+                <li class="text-[20px] text-center font-bold text-white">TEST</li>
+            </ul>
+        </div>
+
     </div>
 </template>
